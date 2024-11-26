6057f4599cff21d18ef25c235bde9560379196bd
hendriktarigann01 <hendriktarigan25@gmail.com>
2024-11-27 02:34:24 +0700

    fix hover

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,27 +4,25 @@ export default function AboutPage() {
   return (
     <section className="relative bg-background flex w-full flex-col items-center justify-evenly py-10 md:h-screen md:flex-row md:overflow-hidden">
       <div className="relative order-2 flex w-3/4 flex-col items-end justify-end md:order-1 md:w-1/3">
-        <div className="my-5 mr-2 text-left font-semibold text-primary-light">
-          <div className="grid w-4/5 grid-cols-4 gap-4">
-            {Skills.map((skill) => (
-              <div
-                key={skill.id}
-                className="skills-icon flex flex-col items-center justify-start group gap-2 h-24"
+        <div className="grid w-4/5 grid-cols-4 gap-4">
+          {Skills.map((skill) => (
+            <div
+              key={skill.id}
+              className="skills-icon flex flex-col items-center justify-start group gap-2 h-auto mb-2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="h-10 w-10 text-primary-light group-hover:text-primary-dark transition-all duration-700 ease-in-out"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="h-10 w-10 text-primary-light group-hover:text-primary-dark transition-all duration-700 ease-in-out"
-                >
-                  <path d={skill.svg} />
-                </svg>
-                <span className="text-sm text-primary-light opacity-0 group-hover:opacity-100 transition-all duration-700 ease-in-out transform group-hover:translate-y-2 w-full text-center">
-                  {skill.name}
-                </span>
-              </div>
-            ))}
-          </div>
+                <path d={skill.svg} />
+              </svg>
+              <span className="text-sm text-primary-light opacity-0 group-hover:opacity-100 transition-all duration-700 ease-in-out transform group-hover:translate-y-2 w-full text-center">
+                {skill.name}
+              </span>
+            </div>
+          ))}
         </div>
       </div>
       <div className="relative order-1 my-10 w-3/4 text-justify md:order-2 md:my-0 md:w-1/4">
