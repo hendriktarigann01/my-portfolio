f1d58f3cbc8d6952133a071ff8e83a3e9fceb9fa
hendriktarigann01 <hendriktarigan25@gmail.com>
2024-12-02 22:23:25 +0700

    responsive about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,8 +3,9 @@ import { Skills } from "@/data/Skills";
 export default function AboutPage() {
   return (
     <section className="relative bg-background flex w-full flex-col items-center justify-evenly py-10 md:h-screen md:flex-row md:overflow-hidden">
-      <div className="relative order-2 flex w-3/4 flex-col items-end justify-end md:order-1 md:w-1/3">
-        <div className="grid w-4/5 grid-cols-4 gap-4">
+      {/* Skill Icons Section */}
+      <div className="relative order-2 mb-24 flex w-3/4 flex-col items-center justify-end md:mb-0 md:order-1 md:w-1/3">
+        <div className="grid w-full grid-cols-2 gap-4 sm:grid-cols-3 md:w-4/5 md:grid-cols-4">
           {Skills.map((skill) => (
             <div
               key={skill.id}
@@ -14,19 +15,21 @@ export default function AboutPage() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="h-10 w-10 font-indie tracking-wider font-semibold group-hover:text-primary-dark transition-all duration-700 ease-in-out"
+                className="h-8 w-8 sm:h-10 sm:w-10 font-indie tracking-wider font-semibold group-hover:text-primary-dark transition-all duration-700 ease-in-out"
               >
                 <path d={skill.svg} />
               </svg>
-              <span className="text-sm font-indie tracking-wider font-semibold opacity-0 group-hover:opacity-100 transition-all duration-700 ease-in-out transform group-hover:translate-y-2 w-full text-center">
+              <span className="text-xs sm:text-sm font-indie tracking-wider font-semibold opacity-0 group-hover:opacity-100 transition-all duration-700 ease-in-out transform group-hover:translate-y-2 w-full text-center">
                 {skill.name}
               </span>
             </div>
           ))}
         </div>
       </div>
-      <div className="relative order-1 my-10 w-3/4 text-justify md:order-2 md:my-0 md:w-1/4">
-        <p className="text-md font-bold font-indie tracking-widest leading-custom">
+
+      {/* About Text Section */}
+      <div className="relative order-1 my-10 w-11/12 text-justify sm:w-4/5 md:order-2 md:my-0 md:w-1/4">
+        <p className="text-sm sm:text-md font-bold font-indie tracking-widest leading-custom">
           "Hello! My name is Muhamad Hendrik Tarigan, you can call me Hendrik,
           Tarigan or whatever you want. I'm a Front-end Developer located in
           West Java, Indonesia. As a Front-end Developer I used to code websites
