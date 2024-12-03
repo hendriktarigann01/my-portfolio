716f762f477f8d58a64fa1c8a61ea17ee0b9c39c
hendriktarigann01 <hendriktarigan25@gmail.com>
2024-12-03 17:50:51 +0700

    wrapper add to deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import "../styles/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-// import ClientWrapper from "@/components/ClientWrapper";
+import ClientWrapper from "@/components/ClientWrapper";
 
 export default function RootLayout({
   children,
@@ -12,13 +12,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="h-screen flex flex-col overflow-visible md:overflow-hidden">
-        {/* <ClientWrapper> */}
+        <ClientWrapper>
         <Header />
         <main className="flex-grow container mx-auto p-4 min-h-screen">
           {children}
         </main>
         <Footer />
-        {/* </ClientWrapper> */}
+        </ClientWrapper>
       </body>
     </html>
   );
